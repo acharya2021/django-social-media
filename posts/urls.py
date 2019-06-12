d7ee51c475679f428e28d04ad05c0dba715dd5d4
Abhisesh Acharya <aachary5@u.rochester.edu>
2019-06-12 18:53:31 -0400

    completed templates for posts

--- posts/urls.py
+++ posts/urls.py
@@ -5,7 +5,7 @@ app_name = 'posts'
 
 urlpatterns = [
     url(r'^$', views.PostList.as_view(), name='all'),
-    url(r'^new/$', views.CreatePost.as_view(), name='all'),
+    url(r'^new/$', views.CreatePost.as_view(), name='create'),
     #     when you click on a user, you should see all their posts
     url(r'^by/(?P<username>[-\w]+)', views.UserPosts.as_view(), name='for_user'),
     url(r'^by/(?P<username>[-\w]+)/(?P<pk>\d+)/$', views.PostDetail.as_view(), name='single'),
